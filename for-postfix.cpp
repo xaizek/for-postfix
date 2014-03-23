2a310d1e3611fc431c9fd374124c051eb1a8f75e
xaizek <xaizek@openmailbox.org>
2014-03-23 21:02:02 +0200

    Enable tool command-line options parsing

--- for-postfix.cpp
+++ for-postfix.cpp
@@ -1,7 +1,17 @@
 #include <cstdlib>
 
+#include <llvm/Support/CommandLine.h>
+
+#include <clang/Tooling/CommonOptionsParser.h>
+
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory toolCategory("for-postfix options");
+
 int
 main(int argc, const char *argv[])
 {
+    CommonOptionsParser optionsParser(argc, argv, toolCategory);
+
     return EXIT_SUCCESS;
 }

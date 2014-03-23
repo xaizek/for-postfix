ea54ae2283a420605364476af59446a4dc82822f
xaizek <xaizek@openmailbox.org>
2014-03-23 23:55:18 +0200

    Match and dump increment operator

--- for-postfix.cpp
+++ for-postfix.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <llvm/Support/CommandLine.h>
 
 #include <clang/ASTMatchers/ASTMatchFinder.h>
@@ -12,6 +14,31 @@ static llvm::cl::OptionCategory toolCategory("for-postfix options");
 
 static llvm::cl::extrahelp commonHelp(CommonOptionsParser::HelpMessage);
 
+static StatementMatcher incMatcher =
+    forStmt(                          // for ([init]; [condition]; [increment])
+        hasIncrement(                 // "increment" part of for-loop
+            unaryOperator(            // any unary op, e.g. *, &, --
+                hasOperatorName("++") // exact unary op: ++
+            ).bind("op")              // bind matched unary op to "op" name
+        )
+    );
+
+class MatchHelper : public MatchFinder::MatchCallback
+{
+public:
+    virtual void run(const MatchFinder::MatchResult &result)
+    {
+        using namespace clang;
+
+        typedef UnaryOperator UnOp;
+
+        if (const UnOp *op = result.Nodes.getNodeAs<UnOp>("op")) {
+            op->dump();
+            std::cout << '\n';
+        }
+    }
+};
+
 int
 main(int argc, const char *argv[])
 {
@@ -19,7 +46,10 @@ main(int argc, const char *argv[])
     ClangTool tool(optionsParser.getCompilations(),
                    optionsParser.getSourcePathList());
 
+    MatchHelper helper;
+
     MatchFinder finder;
+    finder.addMatcher(incMatcher, &helper);
 
     return tool.run(newFrontendActionFactory(&finder));
 }
